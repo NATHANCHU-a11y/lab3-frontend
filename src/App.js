b6284c59ca68cf9e84a9a429f70df153330a11cc
Tianle Zhu <ztl20000828@gmail.com>
2024-02-19 16:13:54 -0500

    added url

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Max from './Component/Max';
 import Min from './Component/Min';
 import CurrentList from './Component/List';
 function App() {
-  const url = 'http://10.20.0.20:8080/bpm'
+  const url = 'http://10.20.0.20:8080/bpm/'
   return (
       <div className="App">
         <Navbar />
